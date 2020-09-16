6ffe44cd25292b9d0619e1b183689ae8c2c16aa3
weihanli <weihan.li@iherb.com>
2020-09-16 16:16:41 +0800

    add some tests

---
+++ CMS.API.Campaign.UnitTests/Services/BannerServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using CMS.API.Campaign.Application.Services;
+using CMS.API.Campaign.Domain.Params;
+using CMS.API.Campaign.Infrastructure.Redis;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Moq;
+using StackExchange.Redis;
+using Xunit;
+
+namespace CMS.API.Campaign.UnitTests.Services
+{
+    public class BannerServiceTest
+    {
+        private readonly IBannerService _bannerService;
+
+        public BannerServiceTest()
+        {
+            var redisMock = new Mock<IRedisAccess>();
+            var redisDbMock = new Mock<IDatabase>();
+            redisDbMock.Setup(x => x.HashGet(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<CommandFlags>()))
+                .Returns(new RedisValue());
+            redisMock.Setup(x => x.GetDatabase()).Returns(redisDbMock.Object);
+
+            var memoryCacheMock = new Mock<IMemoryCache>();
+            memoryCacheMock.Setup(x => x.TryGetValue(It.IsAny<object>(), out It.Ref<object>.IsAny))
+                .Returns(false);
+            memoryCacheMock.Setup(x => x.CreateEntry(It.IsAny<object>())).Returns(new Mock<ICacheEntry>().Object);
+            var cacheService = new CacheService(memoryCacheMock.Object);
+
+            _bannerService = new BannerService(redisMock.Object, cacheService, Options.Create(new RedisConfig()));
+        }
+
+        [Fact]
+        public void GetCampaignBanners()
+        {
+            var result = _bannerService.GetCampaignBanners(new GetBannerSummaries());
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ CMS.API.Campaign.UnitTests/Services/CacheServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using CMS.API.Campaign.Application.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Xunit;
+
+namespace CMS.API.Campaign.UnitTests.Services
+{
+    public class CacheServiceTest
+    {
+        private readonly ICacheService _cacheService;
+
+        public CacheServiceTest()
+        {
+            var memoryCacheMock = new Mock<IMemoryCache>();
+            memoryCacheMock.Setup(x => x.TryGetValue(It.IsAny<object>(), out It.Ref<object>.IsAny))
+                .Returns(false);
+            memoryCacheMock.Setup(x => x.CreateEntry(It.IsAny<object>())).Returns(new Mock<ICacheEntry>().Object);
+            _cacheService = new CacheService(memoryCacheMock.Object);
+        }
+
+        [Fact]
+        public void GetOrCreateTest()
+        {
+            var expectedValue = "iHerb";
+            var result = _cacheService.GetOrSet("Test", () => "iHerb");
+            Assert.Equal(expectedValue, result);
+        }
+    }
+}
