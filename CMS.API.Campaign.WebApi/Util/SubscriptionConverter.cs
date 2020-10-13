87f8b54e290a12ad518817bb8b3107a04b7586fb
Weihan Li <weihan.li@iherb.com>
2020-10-13 19:04:38 +0800

    update SubscriptionConverter

--- CMS.API.Campaign.WebApi/Util/SubscriptionConverter.cs
+++ CMS.API.Campaign.WebApi/Util/SubscriptionConverter.cs
@@ -1,32 +1,14 @@
 ﻿using iHerb.CMS.Cache.Redis;
 using iHerb.CMS.Cache.Redis.Model;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using System;
-using RedisConfig = CMS.API.Campaign.Infrastructure.Redis.RedisConfig;
 
 namespace CMS.API.Campaign
 {
-    internal class CampaignSubscriptionConverter : ISubscriptionConverter
+    internal sealed class CampaignSubscriptionConverter : ISubscriptionConverter
     {
-        private readonly string _setName;
-
-        public CampaignSubscriptionConverter(IOptions<RedisConfig> options)
-        {
-            _setName = options.Value.CampaignBannerSetName;
-        }
-
         public KeyUpdateInfo[] Convert(string message)
         {
-            if (string.IsNullOrEmpty(message))
-                return Array.Empty<KeyUpdateInfo>();
-
-            var info = JsonConvert.DeserializeObject<KeyUpdateInfo[]>(message);
-            foreach (var item in info)
-            {
-                item.SetName = _setName;
-            }
-            return info;
+            return JsonConvert.DeserializeObject<KeyUpdateInfo[]>(message);
         }
     }
 }
